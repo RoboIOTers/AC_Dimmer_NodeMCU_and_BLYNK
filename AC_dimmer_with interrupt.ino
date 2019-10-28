553a30f60b1f476a5187808d6f9d112666d90991
RoboIOTers <46434898+RoboIOTers@users.noreply.github.com>
2019-10-28 15:49:26 +0530

    Update AC_dimmer_with interrupt.ino

--- AC_dimmer_with interrupt.ino
+++ AC_dimmer_with interrupt.ino
@@ -13,7 +13,7 @@ char pass[] = "Your wifi password";    // Set password to "" for open networks.
 
 volatile byte LOAD = 4;    // Output to Optocoupler Triac pin controlling the AC load  (D2 pin in NodeMCU means GPIO04 pin)
                             // you can choose your pin of choice from any GPIO pin in NodeMCU
-volatile int dimming = 100;  // Dimming level (0-100)  0 = ON, 100 = OFF
+volatile int dimming = 128;  // Dimming level (0-128)  0 = ON, 100 = OFF
 volatile byte ZVC=12 ;     // INPUT PIN for the Zero Voltage Cross Signal d6 means GPIO12
 int Slider_Value = 0;
 
@@ -27,9 +27,9 @@ void zero_crosss_int()  //function to be fired at the zero crossing to dim the l
                       // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
                       // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle) 
                       // For 60Hz => 8.33ms (10.000/120)// 10ms=10000us  
-                      // (10000us - 10us) / 100 = 100 (Approx) For 60Hz =>83
+                      // (10000us - 10us) / 128 = 75 (Approx) For 60Hz =>65
   
-  volatile int dimtime = (100*dimming);    // For 60Hz = 83    
+  volatile int dimtime = (75*dimming);    // For 60Hz = 65    
   delayMicroseconds(dimtime);    // Wait till firing the TRIAC    
   digitalWrite(LOAD, HIGH);   // Fire the TRIAC
   delayMicroseconds(10);         // triac On propogation delay 
@@ -46,7 +46,7 @@ ICACHE_RAM_ATTR void setup() {
 void loop() 
 {
   Blynk.run();
-  dimming=mapSlider_Value, 0, 100, 100, 5);
+  dimming=mapSlider_Value, 0, 100, 128, 8);   // at lower values than 8 , flickering may start at 100% so keeping it 8 for avoiding flickering
     delay(10);
     //Serial.println(Slider_Value); 
   
